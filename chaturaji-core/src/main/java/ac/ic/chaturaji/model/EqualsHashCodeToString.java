d9a358103db88fe31a1fc0d291b1f10363fef7b7
Samira Rabbanian <samira.rabanian@gmail.com>
2014-02-05 16:48:35 +0000

    checking the code for junit

--- chaturaji-core/src/main/java/ac/ic/chaturaji/model/EqualsHashCodeToString.java
+++ chaturaji-core/src/main/java/ac/ic/chaturaji/model/EqualsHashCodeToString.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @author jamesdbloom
+ * @author samirarabbanian
  */
 public abstract class EqualsHashCodeToString {
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
