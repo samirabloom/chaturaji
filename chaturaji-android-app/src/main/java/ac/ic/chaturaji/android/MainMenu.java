d60b3e8a3cca14d267ab06e90df7a9fafd77d634
hqazi99 <haiderq343@gmail.com>
2014-02-12 09:41:01 +0000

    Main menu added

--- chaturaji-android-app/src/main/java/ac/ic/chaturaji/android/MainMenu.java
+++ chaturaji-android-app/src/main/java/ac/ic/chaturaji/android/MainMenu.java
@@ -2,12 +2,15 @@ package ac.ic.chaturaji.android;
 
 import ac.ic.chaturaji.chatuService.ChatuService;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainMenu extends Activity {
 
@@ -18,13 +21,42 @@ public class MainMenu extends Activity {
      *                           previously being shut down then this Bundle contains the data it most
      *                           recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
      */
+
+    Button single_player_button;
+    Button multi_player_button;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.main_menu);
 
+        single_player_button = (Button) findViewById(R.id.single_player_button);
+        multi_player_button = (Button) findViewById(R.id.multi_player_button);
+
+        single_player_button.setOnClickListener(singleButtonListener);
+        multi_player_button.setOnClickListener(multiButttonListener);
     }
 
+    public View.OnClickListener singleButtonListener = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View theView) {
+
+            Intent getSingleGame = new Intent(MainMenu.this, GameActivity.class);
+            startActivity(getSingleGame);
+        }
+    };
+
+    public View.OnClickListener multiButttonListener = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View theView) {
+
+            Intent getGameRooms = new Intent(MainMenu.this, GameRoomActivity.class);
+            startActivity(getGameRooms);
+        }
+    };
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
