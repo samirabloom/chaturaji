475c2ffa63516700a750bd39744140780fed6d0d
hqazi99 <haiderq343@gmail.com>
2014-03-10 22:29:21 +0000

    You guessed it... Even more tests

---
+++ chaturaji-android-app-it/src/main/java/ac/ic/chaturaji/android/test/GameRoomTest.java
@@ -0,0 +1,47 @@
+package ac.ic.chaturaji.android.test;
+
+import ac.ic.chaturaji.android.GameRoomActivity;
+import ac.ic.chaturaji.android.GameRoomAdapter;
+import ac.ic.chaturaji.android.R;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ListView;
+
+/**
+ * Created by Haider on 10/03/14.
+ */
+public class GameRoomTest extends ActivityInstrumentationTestCase2<GameRoomActivity> {
+
+    private GameRoomActivity mActivity;
+
+    private GameRoomAdapter localAdapter;
+
+    private ListView localListView;
+
+    public GameRoomTest() {
+        super(GameRoomActivity.class);
+    }
+
+
+    @Override
+    protected void setUp() throws Exception {
+
+        super.setUp();
+
+        setActivityInitialTouchMode(false);
+
+        mActivity = getActivity();
+
+        localListView = (ListView) mActivity.findViewById(R.id.game_rooms_list);
+
+        localAdapter = (GameRoomAdapter) localListView.getAdapter();
+    }
+
+    public void testPreconditions() {
+
+        assertTrue(localListView.getOnItemClickListener() != null);
+
+        assertTrue(localAdapter == null);
+
+    }
+
+}
---
+++ chaturaji-android-app-it/src/main/java/ac/ic/chaturaji/android/test/SettingsActivityTest.java
@@ -0,0 +1,50 @@
+package ac.ic.chaturaji.android.test;
+
+import ac.ic.chaturaji.android.R;
+import ac.ic.chaturaji.android.SettingsActivity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+/**
+ * Created by Haider on 10/03/14.
+ */
+public class SettingsActivityTest extends ActivityInstrumentationTestCase2<SettingsActivity> {
+
+    private SettingsActivity mActivity;
+
+    private CheckBox soundcbx;
+    private Button changePass;
+    private EditText password;
+
+    public SettingsActivityTest() {
+        super(SettingsActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+
+        super.setUp();
+
+        setActivityInitialTouchMode(false);
+
+        mActivity = getActivity();
+
+        soundcbx = (CheckBox) mActivity.findViewById(R.id.soundbox);
+
+        changePass = (Button) mActivity.findViewById(R.id.updateButton);
+
+        password = (EditText) mActivity.findViewById(R.id.newPassword);
+    }
+
+    public void testPreconditions() {
+
+        assertTrue(soundcbx != null);
+
+        assertTrue(changePass != null);
+
+        assertTrue(password != null);
+
+    }
+}
