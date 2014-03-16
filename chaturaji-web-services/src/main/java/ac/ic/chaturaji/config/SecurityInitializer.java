a1a46e44861b9c2d868584601b030f3a3147cf45
Samira Rabbanian <samira.rabanian@gmail.com>
2014-03-16 13:43:28 +0000

    trying to resolve bug in Tomcat's class-loader

--- chaturaji-web-services/src/main/java/ac/ic/chaturaji/config/SecurityInitializer.java
+++
@@ -1,9 +0,0 @@
-package ac.ic.chaturaji.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * @author samirarabbanian
- */
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
